8cbea5edaac26d4c67a877d2a53f31dbfa64cbd8
mrkvon <mrkvon@protonmail.com>
2022-09-10 19:51:08 +0300

    Enable sass, add react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { store } from './app/store'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
+import { store } from './app/store'
 import './index.css'
+import reportWebVitals from './reportWebVitals'
 
 const container = document.getElementById('root')!
 const root = createRoot(container)
@@ -12,7 +13,9 @@ const root = createRoot(container)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 )
