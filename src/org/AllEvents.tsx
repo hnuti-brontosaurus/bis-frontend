f9b5e0d3d877b6858593bfcf2e6c00f088e7fa3d
mrkvon <mrkvon@protonmail.com>
2022-09-29 21:57:03 +0300

    Add layout with header for events lists

---
+++ src/org/AllEvents.tsx
@@ -0,0 +1,5 @@
+const AllEvents = () => {
+  return <div>All Events</div>
+}
+
+export default AllEvents
---
+++ src/org/UnfinishedEvents.tsx
@@ -0,0 +1,5 @@
+const UnifinishedEvents = () => {
+  return <div>Unifinished Events</div>
+}
+
+export default UnifinishedEvents
