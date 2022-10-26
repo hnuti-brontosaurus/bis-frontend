cf15b3daf0690147a262ed10e6b6593f06e20e7a
mrkvon <mrkvon@users.noreply.github.com>
2022-10-26 14:44:55 +0200

    Style form layout (#24)
    Also put basic info from EventForm to a separate file
    Also add Help tooltip

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,1 +1,10 @@
 declare module '@changey/react-leaflet-markercluster'
+
+declare module 'react-tooltip-lite' {
+  import { ReactNode } from 'react'
+  import { TooltipProps as Props } from 'react-tooltip-lite'
+
+  export interface TooltipProps extends Props {
+    children: ReactNode
+  }
+}
