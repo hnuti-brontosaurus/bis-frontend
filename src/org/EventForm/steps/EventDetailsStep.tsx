7436966c947d716ae13458f2704b2c525bf2a249
mrkvon <mrkvon@users.noreply.github.com>
2022-11-09 20:35:04 +0100

    Add rich text component (#36)
    outputs html
    with react-quill

--- src/org/EventForm/steps/EventDetailsStep.tsx
+++ src/org/EventForm/steps/EventDetailsStep.tsx
@@ -1,17 +1,31 @@
-import { useFormContext } from 'react-hook-form'
+import { Controller, useFormContext } from 'react-hook-form'
 import { api } from '../../../app/services/bis'
 import FormInputError from '../../../components/FormInputError'
-import { FormSection, FormSubsection } from '../../../components/FormLayout'
+import {
+  FormSection,
+  FormSubheader,
+  FormSubsection,
+  FullSizeElement,
+} from '../../../components/FormLayout'
 import { ImagesUpload, ImageUpload } from '../../../components/ImageUpload'
 import Loading from '../../../components/Loading'
+import RichTextEditor, {
+  htmlRequired,
+} from '../../../components/RichTextEditor'
 import { getIdsBySlugs } from '../../../utils/helpers'
+import { required } from '../../../utils/validationMessages'
 import { EventFormShape } from '../../EventForm'
 
 const EventDetailsStep = () => {
-  const { register, watch } = useFormContext<EventFormShape>()
+  const { register, watch, control } = useFormContext<EventFormShape>()
   const { data: categories } = api.endpoints.getEventCategories.useQuery()
 
   if (!categories) return <Loading>Připravujeme formulář</Loading>
+
+  const isVolunteering = getIdsBySlugs(categories?.results ?? [], [
+    'public__volunteering__only_volunteering',
+    'public__volunteering__with_experience',
+  ]).includes(+watch('category'))
 
   return (
     <FormSection>
@@ -20,46 +34,87 @@ const EventDetailsStep = () => {
           <input type="text" {...register('internal_note', {})} />
         </FormInputError>
       </FormSubsection>
-      <div>
-        Zvací text: Co nás čeká
-        <FormInputError>
-          <textarea
-            {...register('propagation.invitation_text_introduction', {
-              required: 'required',
-            })}
+      <FullSizeElement>
+        <FormSubheader required>Zvací text: Co nás čeká</FormSubheader>
+        <FormInputError isBlock>
+          <Controller
+            name="propagation.invitation_text_introduction"
+            control={control}
+            rules={{
+              required,
+              validate: {
+                required: htmlRequired(required),
+              },
+            }}
+            render={({ field }) => (
+              <RichTextEditor
+                placeholder="propagation.invitation_text_introduction"
+                {...field}
+              />
+            )}
           />
         </FormInputError>
-      </div>
-      <div>
-        Zvací text: Co, kde a jak
-        <FormInputError>
-          <textarea
-            {...register('propagation.invitation_text_practical_information', {
-              required: 'required',
-            })}
+      </FullSizeElement>
+      <FullSizeElement>
+        <FormSubheader required>Zvací text: Co, kde a jak</FormSubheader>
+        <FormInputError isBlock>
+          <Controller
+            name="propagation.invitation_text_practical_information"
+            control={control}
+            rules={{
+              required,
+              validate: {
+                required: htmlRequired(required),
+              },
+            }}
+            render={({ field }) => (
+              <RichTextEditor
+                placeholder="propagation.invitation_text_practical_information"
+                {...field}
+              />
+            )}
           />
         </FormInputError>
-      </div>
-      <div>
-        Zvací text: Dobrovolnická pomoc
-        <FormInputError>
-          <textarea
-            {...register('propagation.invitation_text_work_description', {
-              required:
-                getIdsBySlugs(categories?.results ?? [], [
-                  'public__volunteering__only_volunteering',
-                  'public__volunteering__with_experience',
-                ]).includes(+watch('category')) && 'Toto pole je povinné!',
-            })}
+      </FullSizeElement>
+      <FullSizeElement>
+        <FormSubheader required={isVolunteering}>
+          Zvací text: Dobrovolnická pomoc
+        </FormSubheader>
+        <FormInputError isBlock>
+          <Controller
+            name="propagation.invitation_text_work_description"
+            control={control}
+            rules={{
+              required: isVolunteering && required,
+              validate: {
+                required: htmlRequired(isVolunteering && required),
+              },
+            }}
+            render={({ field }) => (
+              <RichTextEditor
+                placeholder="propagation.invitation_text_work_description"
+                {...field}
+              />
+            )}
           />
         </FormInputError>
-      </div>
-      <div>
-        Zvací text: Malá ochutnávka
-        <FormInputError>
-          <textarea {...register('propagation.invitation_text_about_us', {})} />
+      </FullSizeElement>
+      <FullSizeElement>
+        <FormSubheader>Zvací text: Malá ochutnávka</FormSubheader>
+        <FormInputError isBlock>
+          <Controller
+            name="propagation.invitation_text_about_us"
+            control={control}
+            rules={{}}
+            render={({ field }) => (
+              <RichTextEditor
+                placeholder="propagation.invitation_text_about_us"
+                {...field}
+              />
+            )}
+          />
         </FormInputError>
-      </div>
+      </FullSizeElement>
       <div>Hlavní foto</div>
       <ImageUpload name="main_image.image" />
       <div>Fotky k malé ochutnávce</div>
