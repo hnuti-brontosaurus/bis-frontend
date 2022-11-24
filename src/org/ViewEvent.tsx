22d4bd66dec9739b9836851aa7e32dda9a0fd9ce
mrkvon <mrkvon@protonmail.com>
2022-11-25 00:12:05 +0100

    Format and style event and opportunity views

--- src/org/ViewEvent.tsx
+++ src/org/ViewEvent.tsx
@@ -1,4 +1,5 @@
 import { skipToken } from '@reduxjs/toolkit/dist/query'
+import { sanitize } from 'dompurify'
 import {
   FaPencilAlt,
   FaRegCalendarAlt,
@@ -12,7 +13,7 @@ import { api } from '../app/services/bis'
 import { Button, ButtonLink } from '../components/Button'
 import Loading from '../components/Loading'
 import { useRemoveEvent } from '../hooks/removeEvent'
-import { formatDateRange } from '../utils/helpers'
+import { formatDateRange, formatDateTime } from '../utils/helpers'
 import styles from './ViewEvent.module.scss'
 
 const ViewEvent = () => {
@@ -23,11 +24,13 @@ const ViewEvent = () => {
     isLoading: isEventLoading,
     isError,
   } = api.endpoints.readEvent.useQuery({ id: eventId })
-
-  const { data: images, isLoading: isImagesLoading } =
-    api.endpoints.readEventImages.useQuery({ eventId })
-  const { data: questions, isLoading: isQuestionsLoading } =
-    api.endpoints.readEventQuestions.useQuery({ eventId })
+  const { data: contactPerson } = api.endpoints.getUser.useQuery(
+    event?.propagation ? { id: event.propagation.contact_person } : skipToken,
+  )
+  const { data: images } = api.endpoints.readEventImages.useQuery({ eventId })
+  const { data: questions } = api.endpoints.readEventQuestions.useQuery({
+    eventId,
+  })
   const [removeEvent, { isLoading: isEventRemoving }] = useRemoveEvent()
   const { data: location } = api.endpoints.readLocation.useQuery(
     event?.location
@@ -44,10 +47,12 @@ const ViewEvent = () => {
 
   if (isEventRemoving) return <Loading>Mažeme akci</Loading>
 
-  const mainImage = [...images.results].sort((a, b) => a.order - b.order)[0]
+  const [mainImage, ...otherImages] = [...images.results].sort(
+    (a, b) => a.order - b.order,
+  )
 
   return (
-    <div>
+    <div className={styles.wrapper}>
       <header className={styles.name}>{event.name}</header>
       <nav className={styles.actions}>
         <ButtonLink success to={`/org/akce/${eventId}/upravit`}>
@@ -83,12 +88,114 @@ const ViewEvent = () => {
           )}
         </div>
       </div>
-      <div>
-        <img src={mainImage.image.medium} alt="" />
+      <div className={styles.infoBoxDetail}>
+        <div className={styles.imageWrapper}>
+          <img className={styles.image} src={mainImage.image.medium} alt="" />
+          <div className={styles.tags}>
+            <div className={styles.tag}>{event.program.name}</div>
+            <div className={styles.tag}>{event.group.name}</div>
+          </div>
+        </div>
+        <table>
+          <tbody>
+            <tr>
+              <th>Datum</th>
+              <td>{formatDateRange(event.start, event.end)}</td>
+            </tr>
+            <tr>
+              <th>Místo</th>
+              <td>
+                {event.online_link ? (
+                  <a
+                    href={event.online_link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    online
+                  </a>
+                ) : (
+                  location?.name
+                )}
+              </td>
+            </tr>
+            <tr>
+              <th>Věk</th>
+              <td>
+                {event.propagation?.minimum_age ?? '?'} -{' '}
+                {event.propagation?.maximum_age ?? '?'} let
+              </td>
+            </tr>
+            <tr>
+              <th>Začátek akce</th>
+              <td>{formatDateTime(event.start)}</td>
+            </tr>
+            <tr>
+              <th>Cena</th>
+              <td>{event.propagation?.cost} Kč</td>
+            </tr>
+            <tr>
+              <th>Kontaktní osoba</th>
+              <td>
+                <div>
+                  {event.propagation?.contact_name ||
+                    contactPerson?.display_name}
+                </div>
+                <div>
+                  {event.propagation?.contact_phone || contactPerson?.phone}
+                </div>
+                <div>
+                  {event.propagation?.contact_email || contactPerson?.email}
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-      <pre>{JSON.stringify(event, null, '  ')}</pre>
-      <pre>{JSON.stringify(images, null, '  ')}</pre>
-      <pre>{JSON.stringify(questions, null, '  ')}</pre>
+      <div className={styles.invitationTexts}>
+        <header>Co na nás čeká</header>
+        <section
+          dangerouslySetInnerHTML={{
+            __html: sanitize(
+              event.propagation?.invitation_text_introduction ?? '',
+            ),
+          }}
+        />
+        <header>Co, kde a jak</header>
+        <section
+          dangerouslySetInnerHTML={{
+            __html: sanitize(
+              event.propagation?.invitation_text_practical_information ?? '',
+            ),
+          }}
+        />
+        <header>Dobrovolnická pomoc</header>
+        <section
+          dangerouslySetInnerHTML={{
+            __html: sanitize(
+              event.propagation?.invitation_text_work_description ?? '',
+            ),
+          }}
+        />
+        <header>Malá ochutnávka</header>
+        <section
+          dangerouslySetInnerHTML={{
+            __html: sanitize(event.propagation?.invitation_text_about_us ?? ''),
+          }}
+        />
+        <div className={styles.imageList}>
+          {otherImages.map(img => (
+            <img
+              className={styles.image}
+              key={img.id}
+              src={img.image.medium}
+              alt=""
+            />
+          ))}
+        </div>
+      </div>
+      <pre className={styles.data}>{JSON.stringify(event, null, '  ')}</pre>
+      <pre className={styles.data}>{JSON.stringify(images, null, '  ')}</pre>
+      <pre className={styles.data}>{JSON.stringify(questions, null, '  ')}</pre>
     </div>
   )
 }
