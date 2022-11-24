7f7165d20f7ff3d9d66b47590fb1d2dd3065e469
mrkvon <mrkvon@protonmail.com>
2022-11-24 09:47:23 +0100

    WIP

--- src/org/ViewEvent.tsx
+++ src/org/ViewEvent.tsx
@@ -1,7 +1,19 @@
-import { Link, useParams } from 'react-router-dom'
+import { skipToken } from '@reduxjs/toolkit/dist/query'
+import {
+  FaPencilAlt,
+  FaRegCalendarAlt,
+  FaRegCheckCircle,
+  FaRegCopy,
+  FaTrashAlt,
+} from 'react-icons/fa'
+import { GrLocation } from 'react-icons/gr'
+import { useParams } from 'react-router-dom'
 import { api } from '../app/services/bis'
+import { Button, ButtonLink } from '../components/Button'
 import Loading from '../components/Loading'
 import { useRemoveEvent } from '../hooks/removeEvent'
+import { formatDateRange } from '../utils/helpers'
+import styles from './ViewEvent.module.scss'
 
 const ViewEvent = () => {
   const params = useParams()
@@ -17,6 +29,13 @@ const ViewEvent = () => {
   const { data: questions, isLoading: isQuestionsLoading } =
     api.endpoints.readEventQuestions.useQuery({ eventId })
   const [removeEvent, { isLoading: isEventRemoving }] = useRemoveEvent()
+  const { data: location } = api.endpoints.readLocation.useQuery(
+    event?.location
+      ? {
+          id: event.location,
+        }
+      : skipToken,
+  )
 
   if (isError) return <>Nepodařilo se nám najít akci</>
 
@@ -25,15 +44,48 @@ const ViewEvent = () => {
 
   if (isEventRemoving) return <Loading>Mažeme akci</Loading>
 
+  const mainImage = [...images.results].sort((a, b) => a.order - b.order)[0]
+
   return (
     <div>
-      <header>There is just data of event here...</header>
-      <nav>
-        <Link to={`/org/akce/${eventId}/upravit`}>upravit</Link>
-        <Link to={`/org/akce/${eventId}/uzavrit`}>po akci</Link>
-        <Link to={`/org/akce/vytvorit?klonovat=${eventId}`}>klonovat</Link>
-        <button onClick={() => removeEvent(event)}>smazat</button>
+      <header className={styles.name}>{event.name}</header>
+      <nav className={styles.actions}>
+        <ButtonLink success to={`/org/akce/${eventId}/upravit`}>
+          <FaPencilAlt /> upravit
+        </ButtonLink>
+        <ButtonLink success to={`/org/akce/${eventId}/uzavrit`}>
+          <FaRegCheckCircle /> po akci
+        </ButtonLink>
+        <ButtonLink success to={`/org/akce/vytvorit?klonovat=${eventId}`}>
+          <FaRegCopy /> klonovat
+        </ButtonLink>
+        <Button danger onClick={() => removeEvent(event)}>
+          <FaTrashAlt /> smazat
+        </Button>
       </nav>
+
+      <div className={styles.infoBox}>
+        <div>
+          <FaRegCalendarAlt /> {formatDateRange(event.start, event.end)}
+        </div>
+        <div>
+          <GrLocation />{' '}
+          {event.online_link ? (
+            <a
+              href={event.online_link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              online
+            </a>
+          ) : (
+            location?.name
+          )}
+        </div>
+      </div>
+      <div>
+        <img src={mainImage.image.medium} alt="" />
+      </div>
       <pre>{JSON.stringify(event, null, '  ')}</pre>
       <pre>{JSON.stringify(images, null, '  ')}</pre>
       <pre>{JSON.stringify(questions, null, '  ')}</pre>
