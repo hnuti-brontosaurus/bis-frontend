7238232cc68bcc574b19b22d23d8162173affd73
mrkvon <mrkvon@users.noreply.github.com>
2022-12-04 01:31:19 +0100

    Implement conditional input for event participants - number or list (#77)
    Also refactor CloseEvent
    Also save and use info that event evidence is finished

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,17 +1,40 @@
 import type { SerializedError } from '@reduxjs/toolkit'
 import type { FetchBaseQueryError } from '@reduxjs/toolkit/query'
 import { ReactNode } from 'react'
+import styles from './Error.module.scss'
 
 const Error = ({
   error,
   children,
+  message,
 }: {
   children?: ReactNode
   error?: FetchBaseQueryError | SerializedError
+  message?: string
 }) => {
+  let name = 'Něco se pokazilo...'
+  let status: number | undefined
+
+  if (message) {
+    name = message
+  } else if (error && 'status' in error) {
+    switch (error.status) {
+      case 404:
+        status = 404
+        name = 'Objekt nenalezen'
+        break
+      case 500:
+        status = 500
+        name = 'Chyba serveru'
+        break
+      default:
+        break
+    }
+  }
   return (
-    <div>
-      <div>Něco se pokazilo...</div>
+    <div className={styles.container}>
+      {status && <div className={styles.status}>{status}</div>}
+      <div className={styles.message}>{name}</div>
       {children}
     </div>
   )
