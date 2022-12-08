69bdb4b2cfad14bd653ce743f86040fcff061569
mrkvon <mrkvon@protonmail.com>
2022-12-08 07:18:58 +0100

    Show error boundary callback with Error component

--- src/config/ErrorBoundaryFallback.tsx
+++ src/config/ErrorBoundaryFallback.tsx
@@ -1,17 +1,19 @@
 // https://docs.sentry.io/platforms/javascript/guides/react/components/errorboundary/#setting-a-fallback-function-render-props
 
 import * as Sentry from '@sentry/react'
+import { Button, Error } from 'components'
 
 const ErrorBoundaryFallback: Sentry.FallbackRender = ({
   error,
   componentStack,
   resetError,
 }) => (
-  <>
+  <Error status={500} error={error}>
     <div>You have encountered an error</div>
     <div>{error.toString()}</div>
     <div>{componentStack}</div>
-    <button
+    <Button
+      success
       onClick={() => {
         /* When resetError() is called it will remove the Fallback component
         and render the Sentry ErrorBoundary's children in their initial state */
@@ -19,8 +21,8 @@ const ErrorBoundaryFallback: Sentry.FallbackRender = ({
       }}
     >
       Click here to reset!
-    </button>
-  </>
+    </Button>
+  </Error>
 )
 
 export default ErrorBoundaryFallback
