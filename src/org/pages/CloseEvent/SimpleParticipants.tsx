37b15991550205f6d236f6d0eaacea9542671e09
mrkvon <mrkvon@protonmail.com>
2023-01-03 08:09:26 +0100

    Change the list into table and add forms for add and edit

--- src/org/pages/CloseEvent/SimpleParticipants.tsx
+++ src/org/pages/CloseEvent/SimpleParticipants.tsx
@@ -1,15 +1,29 @@
-import { EventContact } from 'app/services/bisTypes'
-import { Button, FormInputError, InlineSection } from 'components'
-import { ChangeEventHandler } from 'react'
-import { useFieldArray, useFormContext } from 'react-hook-form'
-import { FaTrash } from 'react-icons/fa'
+import type { EventContact } from 'app/services/bisTypes'
+import classNames from 'classnames'
+import { Button, FormInputError, StyledModal } from 'components'
+import tableStyles from 'org/components/EventForm/steps/ParticipantsStep.module.scss'
+import {
+  ChangeEventHandler,
+  FormHTMLAttributes,
+  useEffect,
+  useState,
+} from 'react'
+import { createPortal } from 'react-dom'
+import {
+  FormProvider,
+  useFieldArray,
+  useForm,
+  useFormContext,
+} from 'react-hook-form'
+import { FaCheck, FaPencilAlt, FaTrash } from 'react-icons/fa'
 import { normalizeKeys } from 'utils/helpers'
 import * as validationMessages from 'utils/validationMessages'
 import * as xlsx from 'xlsx'
 import type { ParticipantsStepFormShape } from './CloseEventForm'
+import styles from './SimpleParticipants.module.scss'
 
 export const SimpleParticipants = () => {
-  const { control, register } = useFormContext<ParticipantsStepFormShape>()
+  const { control } = useFormContext<ParticipantsStepFormShape>()
   const peopleFields = useFieldArray({ control, name: 'record.contacts' })
   const handleUpload: ChangeEventHandler<HTMLInputElement> = e => {
     if (e.target.files && e.target.files.length > 0) {
@@ -31,63 +45,199 @@ export const SimpleParticipants = () => {
       }
     }
   }
+
+  const [open, setOpen] = useState(false)
+  const [defaultValues, setDefaultValues] = useState<EventContact>()
+  const [editPromise, setEditPromise] = useState<{
+    resolve: (data: EventContact) => void
+    reject: (reason?: any) => void
+    promise: Promise<EventContact>
+  }>()
+
+  const getModalData = (data: EventContact) => {
+    let res = undefined
+    let rej = undefined
+
+    const promise = new Promise<EventContact>((resolve, reject) => {
+      res = resolve
+      rej = reject
+    })
+
+    const p = {
+      promise,
+      resolve: res as any,
+      reject: rej as any,
+    }
+
+    setEditPromise(p)
+
+    return promise
+  }
+
+  const handleEdit = async (data: EventContact, i: number) => {
+    setOpen(true)
+    setDefaultValues(data)
+    const finalData = await getModalData(data)
+    peopleFields.update(i, finalData)
+  }
+
+  const handleFinishEdit = (data: EventContact) => {
+    if (editPromise?.resolve) editPromise.resolve(data)
+  }
+
   return (
-    <div>
-      <Button
-        type="button"
-        onClick={() =>
-          peopleFields.prepend({
-            first_name: '',
-            last_name: '',
-          })
-        }
+    <div className={styles.container}>
+      <StyledModal
+        title="Upravit účastníka"
+        open={open}
+        onClose={() => setOpen(false)}
       >
-        Přidat
-      </Button>
-      <input type="file" onChange={handleUpload} />
-      <ul>
-        {peopleFields.fields.map((item, i) => (
-          <li key={item.id}>
-            <InlineSection>
-              <FormInputError>
-                <input
-                  type="text"
-                  placeholder="Jméno*"
-                  {...register(`record.contacts.${i}.first_name` as const, {
-                    required: validationMessages.required,
-                  })}
-                />
-              </FormInputError>
-              <FormInputError>
-                <input
-                  type="text"
-                  placeholder="Příjmení*"
-                  {...register(`record.contacts.${i}.last_name` as const, {
-                    required: validationMessages.required,
-                  })}
-                />
-              </FormInputError>
-              <FormInputError>
-                <input
-                  type="email"
-                  placeholder="E-mail"
-                  {...register(`record.contacts.${i}.email` as const)}
-                />
-              </FormInputError>
-              <FormInputError>
-                <input
-                  type="tel"
-                  placeholder="Telefon"
-                  {...register(`record.contacts.${i}.phone` as const)}
-                />
-              </FormInputError>
-              <Button type="button" onClick={() => peopleFields.remove(i)}>
-                <FaTrash />
-              </Button>
-            </InlineSection>
-          </li>
-        ))}
-      </ul>
+        <SimpleParticipantInput
+          formId="edit-participant-input"
+          defaultValues={defaultValues}
+          onSubmit={data => {
+            handleFinishEdit(data)
+            setOpen(false)
+          }}
+        />
+      </StyledModal>
+      <header>Jednoduchý seznam účastníků</header>
+      <label className={styles.importButton}>
+        Importovat seznam účastníků z excelu
+        <input
+          type="file"
+          accept="application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,text/csv"
+          onChange={handleUpload}
+        />
+      </label>
+      <SimpleParticipantInput onSubmit={data => peopleFields.prepend(data)} />
+      <table className={classNames(tableStyles.table, styles.phoneTable)}>
+        <thead>
+          <tr>
+            <th>Jméno</th>
+            <th>E-mail</th>
+            <th>Telefon</th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {peopleFields.fields.map((item, i) => (
+            <tr
+              key={item.id}
+              title={`${item.first_name || '?'} ${item.last_name || '?'}, ${
+                item.email || '—'
+              }, ${item.phone || '—'}`}
+            >
+              <td>{item.first_name + ' ' + item.last_name}</td>
+              <td>{item.email}</td>
+              <td>{item.phone}</td>
+              <td>
+                <Button type="button" onClick={() => handleEdit(item, i)}>
+                  <FaPencilAlt />
+                </Button>
+              </td>
+              <td>
+                <Button type="button" onClick={() => peopleFields.remove(i)}>
+                  <FaTrash />
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
+}
+
+const SimpleParticipantInput = ({
+  formId = 'simple-participant-form',
+  defaultValues,
+  onSubmit,
+}: {
+  formId?: string
+  defaultValues?: EventContact
+  onSubmit: (value: EventContact) => void
+}) => {
+  const methods = useForm<EventContact>({ defaultValues })
+  const { register, handleSubmit, reset, setFocus } = methods
+
+  // change default values
+  useEffect(() => {
+    if (defaultValues) {
+      reset(defaultValues)
+    }
+  }, [defaultValues, reset, setFocus])
+
+  const handleFormSubmit = handleSubmit(data => {
+    onSubmit(data)
+    setFocus('first_name')
+    reset({ first_name: '', last_name: '', email: '', phone: '' })
+  })
+
+  return (
+    <div>
+      <FormProvider {...methods}>
+        <OutsideForm id={formId} onSubmit={handleFormSubmit} />
+        <FormInputError>
+          <input
+            form={formId}
+            type="text"
+            placeholder="Jméno*"
+            {...register('first_name', {
+              required: validationMessages.required,
+            })}
+          />
+        </FormInputError>
+        <FormInputError>
+          <input
+            form={formId}
+            type="text"
+            placeholder="Příjmení*"
+            {...register('last_name' as const, {
+              required: validationMessages.required,
+            })}
+          />
+        </FormInputError>
+        <FormInputError>
+          <input
+            form={formId}
+            type="email"
+            placeholder="E-mail"
+            {...register('email' as const)}
+          />
+        </FormInputError>
+        <FormInputError>
+          <input
+            form={formId}
+            type="tel"
+            placeholder="Telefon"
+            {...register('phone' as const)}
+          />
+        </FormInputError>
+        <Button primary type="submit" form={formId}>
+          <FaCheck />
+        </Button>
+      </FormProvider>
+    </div>
+  )
+}
+
+const OutsideForm = ({
+  containerId = 'root',
+  ...props
+}: FormHTMLAttributes<HTMLFormElement> & { containerId?: string }) => {
+  const container = document.getElementById(containerId)
+  const el = document.createElement('div')
+
+  useEffect(() => {
+    if (container) {
+      container.appendChild(el)
+      return () => {
+        container.removeChild(el)
+      }
+    }
+  }, [container, el])
+
+  return createPortal(<form {...props} />, el)
 }
