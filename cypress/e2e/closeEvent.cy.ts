3ee4f111409d4d57bf48f2e483d3536134022b24
mrkvon <mrkvon@users.noreply.github.com>
2023-01-06 17:12:12 +0100

    Implement removing participant (#128)
    Show confirmation modal
    Remove participant after confirming
    Test it (implemented test-first)

--- cypress/e2e/closeEvent.cy.ts
+++ cypress/e2e/closeEvent.cy.ts
@@ -1,3 +1,4 @@
+import * as participantsExample from '../fixtures/eventParticipants.json'
 import * as newUserExample from '../fixtures/newUser.json'
 
 describe('Close event - evidence and participants', () => {
@@ -50,67 +51,120 @@ describe('Close event - evidence and participants', () => {
     })
   })
 
-  describe('Add non-existent user as participant', () => {
+  describe('Full participant list', () => {
     beforeEach(() => {
+      // make sure we have a few participants to show
       cy.intercept(
-        { method: 'POST', pathname: '/api/frontend/users' },
-        { fixture: 'newUser' },
-      ).as('createUser')
+        {
+          method: 'GET',
+          pathname: '/api/frontend/events/1000/record/participants/',
+        },
+        { fixture: 'eventParticipants' },
+      )
+
+      // intercept the updates of participants
       cy.intercept(
         { method: 'PATCH', pathname: '/api/frontend/events/1000/' },
         {},
       ).as('updateEvent')
     })
 
-    it('should open a modal, receive data of new user, and save them as a participant', () => {
+    describe('Adding non-existent user as participant', () => {
+      beforeEach(() => {
+        cy.intercept(
+          { method: 'POST', pathname: '/api/frontend/users' },
+          { fixture: 'newUser' },
+        ).as('createUser')
+      })
+
+      it('should open a modal, receive data of new user, and save them as a participant', () => {
+        cy.visit('/org/akce/1000/uzavrit')
+        cy.get('label:contains(Mám všechny informace)')
+          .should('be.visible')
+          .click()
+        cy.get('button:contains(Přidat nového účastníka)')
+          .should('be.visible')
+          .click()
+
+        // we should be able to see a form waiting for user data
+
+        cy.get('input[name=first_name]').should('be.visible').type('first_name')
+        cy.get('input[name=last_name]').should('be.visible').type('last_name')
+        cy.get('[placeholder=DD]').should('be.visible').type('01')
+        cy.get('[placeholder=MM]').should('be.visible').type('01')
+        cy.get('[placeholder=RRRR]').should('be.visible').type('1950')
+        cy.get('[name=email]').type('test@example.com')
+        cy.get('[name=address\\.street]').type('abc')
+        cy.get('[name=address\\.city]').type('de')
+        cy.get('[name=address\\.zip_code]').type('10000')
+        cy.get('[name=address\\.region]').select(3)
+
+        cy.get('[type=submit]:contains(Potvrdit)').click()
+
+        // we should call create user with proper data
+        cy.wait('@createUser')
+          .its('request.body')
+          .should('deep.equal', {
+            first_name: 'first_name',
+            last_name: 'last_name',
+            nickname: '',
+            birth_name: '',
+            sex: null,
+            health_insurance_company: null,
+            health_issues: '',
+            email: 'test@example.com',
+            birthday: '1950-01-01',
+            address: {
+              street: 'abc',
+              city: 'de',
+              zip_code: '10000',
+              region: 3,
+            },
+            contact_address: null,
+            close_person: null,
+            donor: null,
+            offers: null,
+            phone: '',
+          })
+
+        // and we should call update event with proper data
+        cy.wait('@updateEvent')
+          .its('request.body')
+          .should('deep.equal', {
+            record: {
+              participants: [
+                ...participantsExample.results.map(p => p.id),
+                newUserExample.id,
+              ],
+              contacts: [],
+            },
+          })
+      })
+    })
+
+    it('[when clicking remove] should ask for confirmation and remove participant', () => {
+      // go to participants page
       cy.visit('/org/akce/1000/uzavrit')
       cy.get('label:contains(Mám všechny informace)')
         .should('be.visible')
         .click()
-      cy.get('button:contains(Přidat nového účastníka)')
-        .should('be.visible')
-        .click()
 
-      // we should be able to see a form waiting for user data
+      // click delete
+      cy.get('button[aria-label="Smazat účastníka Jana Novak"]').click()
 
-      cy.get('input[name=first_name]').should('be.visible').type('first_name')
-      cy.get('input[name=last_name]').should('be.visible').type('last_name')
-      cy.get('[placeholder=DD]').should('be.visible').type('01')
-      cy.get('[placeholder=MM]').should('be.visible').type('01')
-      cy.get('[placeholder=RRRR]').should('be.visible').type('1950')
-      cy.get('[name=email]').type('test@example.com')
-      cy.get('[name=address\\.street]').type('abc')
-      cy.get('[name=address\\.city]').type('de')
-      cy.get('[name=address\\.zip_code]').type('10000')
-      cy.get('[name=address\\.region]').select(3)
+      // confirmation modal should open
+      cy.contains(
+        'Opravdu chcete smazat účastnici/účastníka Jana Novak z akce Event Name?',
+      )
 
-      cy.get('[type=submit]:contains(Potvrdit)').click()
+      // click Confirm (should be a danger button)
+      cy.get('button[class*=danger]:contains(Ano)').click()
 
-      // we should call create user with proper data
-      cy.wait('@createUser')
-        .its('request.body')
-        .should('deep.equal', {
-          first_name: 'first_name',
-          last_name: 'last_name',
-          nickname: '',
-          birth_name: '',
-          sex: null,
-          health_insurance_company: null,
-          health_issues: '',
-          email: 'test@example.com',
-          birthday: '1950-01-01',
-          address: { street: 'abc', city: 'de', zip_code: '10000', region: 3 },
-          contact_address: null,
-          close_person: null,
-          donor: null,
-          offers: null,
-          phone: '',
-        })
-
-      // and we should call update event with proper data
+      // check that a proper request was sent to api
       cy.wait('@updateEvent')
-        .its('request.body')
-        .should('deep.equal', { record: { participants: [newUserExample.id] } })
+        .its('request.body.record.participants')
+        .should('contain', '11111111-1111-1111-1111-111111111111')
+        .should('not.contain', '00000000-1111-2222-3333-444444444444')
     })
   })
 })
