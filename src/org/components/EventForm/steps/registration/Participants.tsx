52e80fc24cc70a2c7b9ada48640c7eb021bf109a
mrkvon <mrkvon@users.noreply.github.com>
2023-01-06 17:25:38 +0100

    Implement updating participant's data (#129)
    Also add tests (in fact this was implemented test-first)
    
    The flow is still not great, e.g.
      modal with form doesn't reopen when api request fails,
      and there is no indication of progress

--- src/org/components/EventForm/steps/registration/Participants.tsx
+++ src/org/components/EventForm/steps/registration/Participants.tsx
@@ -12,7 +12,7 @@ import {
 } from 'features/systemMessage/useSystemMessage'
 import { useAwaitModal } from 'hooks/useAwaitModal'
 import { merge } from 'lodash'
-import { FC, useState } from 'react'
+import { FC, useMemo, useState } from 'react'
 import { FaTrash as Bin, FaUserEdit as EditUser } from 'react-icons/fa'
 import styles from '../ParticipantsStep.module.scss'
 import { ShowApplicationModal } from './ShowApplicationModal'
@@ -54,11 +54,16 @@ export const Participants: FC<{
 
   const [patchEvent, patchEventStatus] = api.endpoints.updateEvent.useMutation()
   const [createUser, createUserStatus] = api.endpoints.createUser.useMutation()
+  const [updateUser, updateUserStatus] = api.endpoints.updateUser.useMutation()
 
   useShowApiErrorMessage(createUserStatus.error)
+  useShowApiErrorMessage(updateUserStatus.error)
   useShowApiErrorMessage(patchEventStatus.error)
 
-  const [inputNewUserData, newUserModal] = useAwaitModal(TestUserForm)
+  const [inputUserData, userDataModal] = useAwaitModal(UserModalForm, {
+    title: '',
+    props: useMemo(() => ({ user: {} as User }), []),
+  })
   const [confirmRemoveParticipant, removeParticipantModal] = useAwaitModal(
     RemoveParticipantConfirmDialog,
   )
@@ -89,7 +94,10 @@ export const Participants: FC<{
   const handleAddNewParticipant = async () => {
     // first we open a modal with new user form
     try {
-      const inputData = await inputNewUserData({ title: 'Nový účastník' })
+      const inputData = await inputUserData({
+        title: 'Nový účastník',
+        props: { user: {} as User },
+      })
       const data = merge({ donor: null, offers: null }, inputData)
       // then we create the user and add them as participant
       const { id: userId } = await createUser(data).unwrap()
@@ -97,6 +105,18 @@ export const Participants: FC<{
     } catch (e) {
       // not sure if we need to do anything here
       // one case is when adding is cancelled
+    }
+  }
+
+  const handleEditParticipant = async (participant: User) => {
+    try {
+      const inputData = await inputUserData({
+        title: `Úprava dat účastnice/účastníka ${participant.first_name} ${participant.last_name}`,
+        props: { user: participant },
+      })
+      await updateUser({ id: participant.id, patchedUser: inputData }).unwrap()
+    } catch (e) {
+      // do something with error?
     }
   }
 
@@ -128,7 +148,7 @@ export const Participants: FC<{
 
   return (
     <div className={styles.ListContainer}>
-      {newUserModal}
+      {userDataModal}
       {removeParticipantModal}
       <h2>Účastníci</h2>
       {!isReadParticipantsLoading ? (
@@ -198,7 +218,13 @@ export const Participants: FC<{
                       onClick={() => {}}
                       className={stylesTable.cellWithButton}
                     >
-                      <EditUser className={styles.editUserIconContainer} />
+                      <button
+                        type="button"
+                        aria-label={`Upravit účastníka ${participant.first_name} ${participant.last_name}`}
+                        onClick={() => handleEditParticipant(participant)}
+                      >
+                        <EditUser className={styles.editUserIconContainer} />
+                      </button>
                     </td>
                     <td
                       onClick={() => {}}
@@ -245,13 +271,15 @@ export const Participants: FC<{
   )
 }
 
-const TestUserForm = ({
+const UserModalForm = ({
   onResolve,
   onReject,
+  user,
 }: {
   onResolve: (user: UserPayload) => void
   onReject: () => void
-}) => <UserForm onSubmit={onResolve} onCancel={onReject} />
+  user: User
+}) => <UserForm initialData={user} onSubmit={onResolve} onCancel={onReject} />
 
 const RemoveParticipantConfirmDialog = ({
   onResolve,
