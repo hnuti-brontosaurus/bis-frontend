482f3f63313a199a5e6256d9e26a52e7aa7ff5c9
Talita Dzikzlasu <startingcommunity@proton.me>
2023-01-07 14:57:55 +0100

    Style prticipnats buttons, first iteration (#138)
    Co-authored-by: Talita Andrzejewska <dzikilubiabloto@protonmail.com>

--- src/components/ButtonSelect/ButtonSelect.tsx
+++ src/components/ButtonSelect/ButtonSelect.tsx
@@ -1,19 +1,26 @@
 import classNames from 'classnames'
-import { ForwardedRef, forwardRef, InputHTMLAttributes, ReactNode } from 'react'
+import {
+  ForwardedRef,
+  forwardRef,
+  InputHTMLAttributes,
+  ReactElement,
+  ReactNode,
+} from 'react'
 import styles from './ButtonSelect.module.scss'
 
 interface ButtonSelectProps extends InputHTMLAttributes<HTMLInputElement> {
   label: ReactNode
   id: string
+  icon: ReactElement<any, any>
 }
 
 export const ButtonSelect = forwardRef(
   (
-    { id, label, ...rest }: ButtonSelectProps,
+    { id, label, icon, ...rest }: ButtonSelectProps,
     ref: ForwardedRef<HTMLInputElement>,
   ) => {
     return (
-      <>
+      <div className={styles.buttonSelect}>
         <input
           type="radio"
           id={id}
@@ -22,9 +29,11 @@ export const ButtonSelect = forwardRef(
           className={styles.input}
         />
         <label htmlFor={id} className={styles.label}>
-          {label}
+          {/* @ts-ignore */}
+          <div className={styles.icon}>{icon}</div>
+          <div className={styles.labelText}>{label}</div>
         </label>
-      </>
+      </div>
     )
   },
 )
