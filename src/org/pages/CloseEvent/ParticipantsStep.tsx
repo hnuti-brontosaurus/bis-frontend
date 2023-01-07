d574a53b40b4229e49ff89ec4c6fd60acf4db167
Talita Andrzejewska <dzikilubiabloto@protonmail.com>
2023-01-07 14:57:25 +0100

    Style prticipnats buttons, first iteration

--- src/org/pages/CloseEvent/ParticipantsStep.tsx
+++ src/org/pages/CloseEvent/ParticipantsStep.tsx
@@ -1,4 +1,7 @@
 import { Event } from 'app/services/bisTypes'
+import { ReactComponent as Counted } from 'assets/counting.svg'
+import { ReactComponent as EmailList } from 'assets/email-list.svg'
+import { ReactComponent as FullList } from 'assets/full-list.svg'
 import {
   ButtonSelect,
   FormInputError,
@@ -9,7 +12,7 @@ import {
 } from 'components'
 import { ButtonSelectGroup } from 'components/ButtonSelect/ButtonSelect'
 import { ParticipantsStep as ParticipantsList } from 'org/components/EventForm/steps/ParticipantsStep'
-import { useEffect } from 'react'
+import { ReactElement, useEffect } from 'react'
 import { FormProvider, UseFormReturn } from 'react-hook-form'
 import { Entries } from 'type-fest'
 import { required } from 'utils/validationMessages'
@@ -22,10 +25,16 @@ import { SimpleParticipants } from './SimpleParticipants'
 
 type ParticipantInputType = CloseEventFormShape['participantInputType']
 
-const optionButtonConfig: Record<ParticipantInputType, string> = {
-  count: 'Mám jen počet účastníků',
-  'simple-list': 'Mám jen jméno + příjmení + email',
-  'full-list': 'Mám všechny informace',
+const optionButtonConfig: Record<
+  ParticipantInputType,
+  { label: string; icon: ReactElement<any, any> }
+> = {
+  count: { label: 'Mám jen počet účastníků', icon: <Counted /> },
+  'simple-list': {
+    label: 'Mám jen jméno + příjmení + email',
+    icon: <EmailList />,
+  },
+  'full-list': { label: 'Mám všechny informace', icon: <FullList /> },
 } as const
 
 export const ParticipantsStep = ({
@@ -70,7 +79,7 @@ export const ParticipantsStep = ({
                     Object.entries(optionButtonConfig) as Entries<
                       typeof optionButtonConfig
                     >
-                  ).map(([value, label]) => (
+                  ).map(([value, { label, icon }]) => (
                     <ButtonSelect
                       key={value}
                       id={value}
@@ -79,72 +88,79 @@ export const ParticipantsStep = ({
                         required: 'Vyberte, prosím, jednu z možností',
                       })}
                       value={value}
+                      icon={icon}
                     />
                   ))}
                 </ButtonSelectGroup>
               </FormInputError>
             )}
-            {!areParticipantsRequired &&
-              (inputType === 'count' || inputType === 'simple-list') && (
-                <>
-                  <InlineSection>
-                    <Label required>Počet účastníků celkem</Label>
-                    <FormInputError>
-                      <input
-                        type="number"
-                        min={0}
-                        placeholder="number_of_participants"
-                        {...register('record.number_of_participants', {
-                          required,
-                          min: {
-                            value: 0,
-                            message: 'Hodnota musí být větší nebo rovna 0',
-                          },
-                        })}
-                      />
-                    </FormInputError>
-                  </InlineSection>
-                  <InlineSection>
-                    <Label required>Z toho počet účastníků do 26 let</Label>
-                    <FormInputError>
-                      <input
-                        type="number"
-                        min={0}
-                        placeholder="number_of_participants_under_26"
-                        {...register('record.number_of_participants_under_26', {
-                          required,
-                          min: {
-                            value: 0,
-                            message: 'Hodnota musí být větší nebo rovna 0',
-                          },
-                          validate: {
-                            lessThanParticipants: value => {
-                              const numberOfParticipants = Number(
-                                watch('record.number_of_participants'),
-                              )
-                              const numberOfParticipantsUnder26 = Number(value)
-
-                              return (
-                                (!isNaN(numberOfParticipantsUnder26) &&
-                                  !isNaN(numberOfParticipants) &&
-                                  numberOfParticipantsUnder26 <=
-                                    numberOfParticipants) ||
-                                'Hodnota nesmí být větší než počet účastníků celkem'
-                              )
+            <div className={styles.tabFrame}>
+              {!areParticipantsRequired &&
+                (inputType === 'count' || inputType === 'simple-list') && (
+                  <>
+                    <InlineSection>
+                      <Label required>Počet účastníků celkem</Label>
+                      <FormInputError>
+                        <input
+                          type="number"
+                          min={0}
+                          placeholder="number_of_participants"
+                          {...register('record.number_of_participants', {
+                            required,
+                            min: {
+                              value: 0,
+                              message: 'Hodnota musí být větší nebo rovna 0',
                             },
-                          },
-                        })}
-                      />
-                    </FormInputError>
-                  </InlineSection>
-                </>
+                          })}
+                        />
+                      </FormInputError>
+                    </InlineSection>
+                    <InlineSection>
+                      <Label required>Z toho počet účastníků do 26 let</Label>
+                      <FormInputError>
+                        <input
+                          type="number"
+                          min={0}
+                          placeholder="number_of_participants_under_26"
+                          {...register(
+                            'record.number_of_participants_under_26',
+                            {
+                              required,
+                              min: {
+                                value: 0,
+                                message: 'Hodnota musí být větší nebo rovna 0',
+                              },
+                              validate: {
+                                lessThanParticipants: value => {
+                                  const numberOfParticipants = Number(
+                                    watch('record.number_of_participants'),
+                                  )
+                                  const numberOfParticipantsUnder26 =
+                                    Number(value)
+
+                                  return (
+                                    (!isNaN(numberOfParticipantsUnder26) &&
+                                      !isNaN(numberOfParticipants) &&
+                                      numberOfParticipantsUnder26 <=
+                                        numberOfParticipants) ||
+                                    'Hodnota nesmí být větší než počet účastníků celkem'
+                                  )
+                                },
+                              },
+                            },
+                          )}
+                        />
+                      </FormInputError>
+                    </InlineSection>
+                  </>
+                )}
+              {!areParticipantsRequired && inputType === 'simple-list' && (
+                <SimpleParticipants />
               )}
-            {!areParticipantsRequired && inputType === 'simple-list' && (
-              <SimpleParticipants />
-            )}
-            {(areParticipantsRequired || inputType === 'full-list') && (
-              <ParticipantsList eventId={event.id} eventName={event.name} />
-            )}
+              {(areParticipantsRequired || inputType === 'full-list') && (
+                <ParticipantsList eventId={event.id} eventName={event.name} />
+              )}
+            </div>
           </FormSection>
         </FormSectionGroup>
       </form>
