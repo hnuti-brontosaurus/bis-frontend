0cc4f08004840e7b30b13cbdca89a0efa09e6386
mrkvon <mrkvon@users.noreply.github.com>
2023-01-09 13:28:54 +0100

    Lazy load Quill and Leaflet with React.lazy (#147)
    These were the largest libraries left in the bundle
    Now they load separately when they're actually needed
    So the app can load faster at the beginning
    
    You can run `yarn analyze` to see how libraries contribute to bundles

--- src/components/RichTextEditor/RichTextEditor.tsx
+++ src/components/RichTextEditor/RichTextEditor.tsx
@@ -1,26 +1,28 @@
-import { forwardRef } from 'react'
+import React, { forwardRef, Suspense } from 'react'
 import { ControllerRenderProps } from 'react-hook-form'
-import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.core.css'
 import 'react-quill/dist/quill.snow.css'
 import { stripHtml } from 'utils/helpers'
 import styles from './RichTextEditor.module.scss'
+const ReactQuill = React.lazy(() => import('react-quill'))
 
 export const RichTextEditor = forwardRef<
   any,
   ControllerRenderProps<any, string> & { placeholder?: string }
 >((props, ref) => (
-  <ReactQuill
-    className={styles.wrapper}
-    theme="snow"
-    modules={{
-      toolbar: ['bold', { list: 'ordered' }, { list: 'bullet' }, 'link'],
-    }}
-    formats={['bold', 'list', 'bullet', 'link']}
-    {...props}
-    id={props.name}
-    ref={ref}
-  />
+  <Suspense>
+    <ReactQuill
+      className={styles.wrapper}
+      theme="snow"
+      modules={{
+        toolbar: ['bold', { list: 'ordered' }, { list: 'bullet' }, 'link'],
+      }}
+      formats={['bold', 'list', 'bullet', 'link']}
+      {...props}
+      id={props.name}
+      ref={ref}
+    />
+  </Suspense>
 ))
 
 // TODO rewrite to yay schema
