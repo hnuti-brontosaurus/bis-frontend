4faa49803b10392247ebfe3eab44eadba64322d4
Talita Andrzejewska <dzikilubiabloto@protonmail.com>
2023-01-10 20:20:46 +0100

    Comment out tests

--- cypress/e2e/forbidCreateEvent.cy.ts
+++ cypress/e2e/forbidCreateEvent.cy.ts
@@ -1,51 +1,57 @@
-describe('create event', () => {
-  // stub api endpoints before each request
-  beforeEach(() => {
-    cy.intercept(
-      { method: 'GET', pathname: '/api/categories/qualification_categories/' },
-      { fixture: 'qualificationCategories' },
-    )
-    cy.intercept('POST', '/api/auth/login/', { token: '1234567890abcdef' })
-    cy.intercept('GET', '/api/auth/whoami/', {
-      id: '0419781d-06ba-432b-8617-797ea14cf848',
-    })
-    cy.intercept(
-      { method: 'GET', path: /\/api\/frontend\/users\/[a-zA-Z0-9-]+\// },
-      { fixture: 'organizerWithoutQualifications' },
-    )
-  })
+/* The tests below is commented out because at the moment,
+      we want to allow every organizer to create actions
+      To make it work again (stop organizers without qualifications to create events), 
+      just uncomment the file
+      */
 
-  // sign in before each request
-  beforeEach(() => {
-    // https://xkcd.com/936/
-    cy.login('asdf@example.com', 'correcthorsebatterystaple')
-  })
+// describe('create event', () => {
+//   // stub api endpoints before each request
+//   beforeEach(() => {
+//     cy.intercept(
+//       { method: 'GET', pathname: '/api/categories/qualification_categories/' },
+//       { fixture: 'qualificationCategories' },
+//     )
+//     cy.intercept('POST', '/api/auth/login/', { token: '1234567890abcdef' })
+//     cy.intercept('GET', '/api/auth/whoami/', {
+//       id: '0419781d-06ba-432b-8617-797ea14cf848',
+//     })
+//     cy.intercept(
+//       { method: 'GET', path: /\/api\/frontend\/users\/[a-zA-Z0-9-]+\// },
+//       { fixture: 'organizerWithoutQualifications' },
+//     )
+//   })
 
-  it('shows a gray "NOVA AKCE" button that is disabled', () => {
-    cy.visit('/org/')
-    cy.location('pathname').should('equal', '/org/')
+//   // sign in before each request
+//   beforeEach(() => {
+//     // https://xkcd.com/936/
+//     cy.login('asdf@example.com', 'correcthorsebatterystaple')
+//   })
 
-    // check color of the button
-    cy.get('a[id=createEvent]')
-      .should('have.css', 'background-color')
-      .and('eq', 'rgb(244, 248, 250)')
-    cy.get('a[id=createEvent]')
-      .should('have.css', 'pointer-events')
-      .and('eq', 'none')
-  })
+//   it('shows a gray "NOVA AKCE" button that is disabled', () => {
+//     cy.visit('/org/')
+//     cy.location('pathname').should('equal', '/org/')
 
-  it("can't fill form and send data to create a new event", () => {
-    cy.visit('/org/akce/vytvorit')
-    cy.location('pathname').should('equal', '/org/akce/vytvorit')
+//     // check color of the button
+//     cy.get('a[id=createEvent]')
+//       .should('have.css', 'background-color')
+//       .and('eq', 'rgb(244, 248, 250)')
+//     cy.get('a[id=createEvent]')
+//       .should('have.css', 'pointer-events')
+//       .and('eq', 'none')
+//   })
 
-    // check that error message was shown
-    cy.contains('Nemáš dostatečná práva k TODO text organizátorskému přístupu')
-  })
+//   it("can't fill form and send data to create a new event", () => {
+//     cy.visit('/org/akce/vytvorit')
+//     cy.location('pathname').should('equal', '/org/akce/vytvorit')
 
-  it("can't clone an event", () => {
-    cy.visit('/org/akce/vytvorit?klonovat=1000')
+//     // check that error message was shown
+//     cy.contains('Nemáš dostatečná práva k TODO text organizátorskému přístupu')
+//   })
 
-    // check that error message was shown
-    cy.contains('Nemáš dostatečná práva k TODO text organizátorskému přístupu')
-  })
-})
+//   it("can't clone an event", () => {
+//     cy.visit('/org/akce/vytvorit?klonovat=1000')
+
+//     // check that error message was shown
+//     cy.contains('Nemáš dostatečná práva k TODO text organizátorskému přístupu')
+//   })
+// })
