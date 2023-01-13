189062ee87eeeaf6b95d39b92e7fb5f780b8d4eb
mrkvon <mrkvon@users.noreply.github.com>
2023-01-13 05:12:21 +0100

    Disable unimplemented buttons on user Home page (#161)
    ...based on work done in #123 by @dzikudzik @regenerativecommunity

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,5 @@
 import illustration from 'assets/happy-earth-TODO-replace-with-original.webp'
 import classNames from 'classnames'
-import { useAllowedToCreateEvent } from 'hooks/useAllowedToCreateEvent'
 import { ReactNode } from 'react'
 import { Link, To } from 'react-router-dom'
 import styles from './Home.module.scss'
@@ -19,32 +18,26 @@ export interface HomeButtonConfig {
     | 'simple'
 }
 
-export const Home = ({ buttons }: { buttons: HomeButtonConfig[] }) => {
-  const [canAddEvent, isLoading] = useAllowedToCreateEvent()
-
-  return (
-    <div className={styles.container}>
-      {!isLoading && (
-        <nav className={styles.mainMenu}>
-          {buttons.map(({ title, detail, link, theme }) => (
-            <Link
-              to={canAddEvent || theme !== 'createEvent' ? link : ''}
-              key={title}
-              className={classNames(
-                styles.menuItem,
-                styles[theme],
-                !canAddEvent && theme === 'createEvent' && styles.disabled,
-              )}
-              aria-disabled={!canAddEvent && theme === 'createEvent'}
-              id={theme}
-            >
-              <header className={styles.title}>{title}</header>
-              <div className={styles.detail}>{detail}</div>
-            </Link>
-          ))}
-        </nav>
-      )}
-      <img className={styles.illustration} src={illustration} alt="" />
-    </div>
-  )
-}
+export const Home = ({ buttons }: { buttons: HomeButtonConfig[] }) => (
+  <div className={styles.container}>
+    <nav className={styles.mainMenu}>
+      {buttons.map(({ title, detail, link, theme }) => (
+        <Link
+          to={link}
+          key={title}
+          className={classNames(
+            styles.menuItem,
+            styles[theme],
+            !link && styles.disabled,
+          )}
+          aria-disabled={!link}
+          id={theme}
+        >
+          <header className={styles.title}>{title}</header>
+          <div className={styles.detail}>{detail}</div>
+        </Link>
+      ))}
+    </nav>
+    <img className={styles.illustration} src={illustration} alt="" />
+  </div>
+)
