55948259b586d9b313832b73b463e73040b1dbfe
mrkvon <mrkvon@protonmail.com>
2023-01-17 18:38:02 +0100

    Change links in footer, refactor
    https://docs.google.com/document/d/1O17qUylQ5EZZJgu3g6t8FePOzK0VAY-a_9TqYGbuhG4/edit

---
+++ src/config/static/footer.ts
@@ -0,0 +1,27 @@
+export const footerLinks: { name: string; url: string }[] = [
+  {
+    name: 'Mozek',
+    url: 'https://mozek.brontosaurus.cz/index.php/hemisfera-orgu/dokumenty/pro-organizatory',
+  },
+  { name: 'Zapoj se', url: 'https://brontosaurus.cz/zapoj-se/' },
+  {
+    name: 'Mapa lokalit',
+    url: 'https://www.mapotic.com/lokality-hnuti-brontosaurus',
+  },
+  {
+    name: 'Databáze budek',
+    url: 'http://peceoprirodu.cz/databaze/sign/in?backlink=jpop9',
+  },
+  {
+    name: 'Zpětná vazba',
+    url: 'https://zpetna-vazba.brontosaurus.cz/login.php',
+  },
+  {
+    name: 'Mentoři',
+    url: 'https://mozek.brontosaurus.cz/index.php/cely-mozek/organizovani-akci/mentorsky-program',
+  },
+  { name: 'Staň se členem', url: 'https://brontosaurus.cz/zapoj-se/clenstvi/' },
+  { name: 'Adoptuj Brontosaura', url: 'https://www.darujme.cz/projekt/525' },
+]
+
+export const footerText = '...footer text...'
