041e2eb6b883b73735a34cf2c51ff15db7736219
Talita Dzikzlasu <startingcommunity@proton.me>
2023-02-28 09:59:55 +0100

    Add pdf print

---
+++ src/org/components/EventForm/steps/registration/participantsPdf/text.ts
@@ -0,0 +1,14 @@
+/** Podmínky */
+export const conditionsTitle = 'Podmínky zpracování osobních údajů'
+export const conditions =
+  'Osobní údaje jsou poskytovány správcům osobních údajů, jimiž jsou Hnutí Brontosaurus (HB) a shora uvedená pobočná organizační jednotka HB. Účelem zpracování osobních údajů je zajištění činnosti HB, evidence účastníků, jejich kontaktování za účelem poskytování informací o činnosti HB a za účelem splnění povinností vyplývajících z právních předpisů a stanovených podmínek pro poskytnutí finanční aj. podpory, popřípadě další účely specifikované v níže uvedených podmínkách.\nPodrobné informace o správcích osobních údajů, účelech a způsobu jejich zpracování a o vašich právech, které může vůči zpracovateli vznést, lze najít na internetové adrese http://www.brontosaurus.cz/osobni-udaje popř. budou na vyžádání poskytnuty při podpisu přihlášky.\nSvým podpisem níže stvrzuji, že jsem se seznámil(a) s podmínkami zpracování svých osobních údajů. Dále stvrzuji, že jsem uvedené osobní údaje poskytl(a) osobně, popřípadě jakožto oprávněný zákonný zástupce osoby mladší 15 let.\nSvým podpisem uděluji souhlas s tím, aby fotografie, videozáznam či zvukový záznam akce, na němž je zachycena má podobizna nebo jiné projevy osobní povahy při činnosti v rámci Hnutí Brontosaurus, mohly být použity za účelem dokumentace a propagace činnosti správců osobních údajů. Stejný souhlas uděluji jako zákonný zástupce dítěte za toto dítě. Tento souhlas je kdykoli odvolatelný způsobem uvedeným v odkazovaných podmínkách zpracování osobních údajů.'
+
+/** Bezpečnost */
+export const safetyTitle = 'Bezpečnost práce a ochrana zdraví'
+export const safety =
+  'Dále potvrzuji, že jsem byl(a) proškolen(a) o zásadách bezpečnosti při všech činnostech na akci, o hygienických a protipožárních předpisech a režimu na akci a uplatňovaných provozních a organizačních zásadách.'
+
+/** Prohlášení */
+// export const declarationTitle = 'Prohlášení: '
+// export const declaration =
+//   'Podpisem čestně prohlašuji, že mnou níže uvedené údaje jsou pravdivé.'
